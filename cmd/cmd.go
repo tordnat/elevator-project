6563f3ae148b8a4464225b41b1d2345681648536
sveinisdahl <sveinisdahl1@gmail.com>
2024-03-11 21:50:00 +0100

    Added support for jonas

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -8,8 +8,8 @@ import (
 func InitCommandLineArgs(args []string) (int, string) {
 	var port int
 	flag.IntVar(&port, "port", 15657, "Specifies the port to connect to elevator on, default is 15657")
-	var id int
-	flag.IntVar(&id, "id", 0, "Specifies the unique id of elevator, default is 0")
+	var id string
+	flag.StringVar(&id, "id", "0", "Specifies the unique id of elevator, default is 0")
 	flag.Parse()
 
 	fmt.Println("Port for connecting to elevator is", port, " ID of elevator is ", id)
