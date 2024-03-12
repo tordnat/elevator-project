d18356d68d8659acff86e17f9bcc1ca34d58b3fc
tordnat <tordna@protonmail.com>
2024-03-12 14:55:46 +0100

    Edit install script

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -9,4 +9,4 @@ sudo chmod +x Project-resources/cost_fns/hall_request_assigner/build.sh
 cd Project-resources/cost_fns/hall_request_assigner/
 ./build.sh
 cd $WORKING_DIR
-sudo cp Project-resources/cost_fns/hall_request_assigner $HRA_INSTALL_PATH
+sudo mv Project-resources/cost_fns/hall_request_assigner $HRA_INSTALL_PATH
