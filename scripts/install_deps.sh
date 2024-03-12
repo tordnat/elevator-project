1a2229149a335021b5bdccb5eade4f507a86a4c4
tordnat <tordna@protonmail.com>
2024-03-12 15:16:02 +0100

    Fix path in install script

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HRA_INSTALL_PATH="elevatorAlgorithm/hra/"
+HRA_INSTALL_PATH="elevatorAlgorithm/hra"
 echo "Building project and moving hall_request_assigner to $HRA_INSTALL_PATH"
 
 WORKING_DIR=$(pwd)
@@ -9,5 +9,5 @@ sudo chmod +x Project-resources/cost_fns/hall_request_assigner/build.sh
 cd Project-resources/cost_fns/hall_request_assigner/
 ./build.sh
 cd $WORKING_DIR
-sudo mv Project-resources/cost_fns/hall_request_assigner $HRA_INSTALL_PATH
+sudo mv Project-resources/cost_fns/hall_request_assigner $HRA_INSTALL_PATH/
 sudo chmod +x $HRA_INSTALL_PATH/hall_request_assigner
