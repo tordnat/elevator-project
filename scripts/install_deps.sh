2df569fc60086153b1a9e1eb29cc77ab73b55745
tordnat <tordna@protonmail.com>
2024-03-12 14:51:12 +0100

    Edit install script

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -3,5 +3,6 @@
 HRA_INSTALL_PATH="elevatorAlgorithm/hra/"
 echo "Building project and moving hall_request_assigner to $HRA_INSTALL_PATH"
 
+sudo chmod +x Project-resources/cost_fns/hall_request_assigner/build.sh
 Project-resources/cost_fns/hall_request_assigner/build.sh
 sudo cp Project-resources/cost_fns/hall_request_assigner $HRA_INSTALL_PATH
